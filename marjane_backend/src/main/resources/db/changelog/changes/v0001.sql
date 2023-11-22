f78293a0dcbbed9a4e96609d815ae3c0cf5e799e
najoua belhaj <najouabelhaj7@gmail.com>
2023-11-22 09:07:33 +0100

    change

--- marjane_backend/src/main/resources/db/changelog/changes/v0001.sql
+++
@@ -1,6 +0,0 @@
-create table newtable (
-                          id bigserial not null,
-                          question bigint not null,
-                          description text not null,
-                          primary key (id)
-);
